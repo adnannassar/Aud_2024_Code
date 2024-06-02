a6f5a2ae0bebaa62c6af6adb1cefc906467cd710
adnan <adnan.nassar90@gmail.com>
2024-06-02 14:14:58 +0200

    added: Linked list teil 1

--- src/LinkedList/SimpleLinkedList/Main.java
+++ src/LinkedList/SimpleLinkedList/Main.java
@@ -3,15 +3,32 @@ package LinkedList.SimpleLinkedList;
 public class Main {
     public static void main(String[] args) {
         SimpleLinkedList simpleLinkedList = new SimpleLinkedList();
-
+        System.out.println("Before Add: ");
+        System.out.println("isEmpty() --> " + simpleLinkedList.isEmpty());
+        System.out.println("Get Value from index (0) --> " + simpleLinkedList.getValueByIndex(0)); // -1
+        System.out.println("Get Index by value (0) --> " + simpleLinkedList.getValueByIndex(0)); // -1
         simpleLinkedList.add(1);
         simpleLinkedList.add(2);
         simpleLinkedList.add(3);
         simpleLinkedList.add(4);
         simpleLinkedList.add(5);
         simpleLinkedList.add(6);
-
-
+        System.out.println("\nAfter Add: ");
+        System.out.println("isEmpty() --> " + simpleLinkedList.isEmpty());
         simpleLinkedList.print();
+
+        System.out.println("Search (1) --> " + simpleLinkedList.search(1)); // O(1)
+        System.out.println("Search (3) --> " + simpleLinkedList.search(3)); // O(n)
+        System.out.println("Search (7) --> " + simpleLinkedList.search(6)); // O(n)
+
+        System.out.println("\nGet Value from index (0) --> " + simpleLinkedList.getValueByIndex(0)); // 1
+        System.out.println("Get Value from index (3) --> " + simpleLinkedList.getValueByIndex(3)); // 4
+        System.out.println("Get Value from index (5) --> " + simpleLinkedList.getValueByIndex(5)); // 6
+        System.out.println("Get Value from index (200) --> " + simpleLinkedList.getValueByIndex(200)); // -1
+        System.out.println("Get Value from index (-10) --> " + simpleLinkedList.getValueByIndex(-10)); // -1
+
+        System.out.println("\nGet Index by value (1) --> " + simpleLinkedList.getIndexByValue(1)); // 0 --> O(1)
+        System.out.println("Get Index by value (3) --> " + simpleLinkedList.getIndexByValue(3)); // 2
+        System.out.println("Get Index by value (100) --> " + simpleLinkedList.getIndexByValue(100)); // -1
     }
 }
--- src/LinkedList/SimpleLinkedList/SimpleLinkedList.java
+++ src/LinkedList/SimpleLinkedList/SimpleLinkedList.java
@@ -1,36 +1,114 @@
 package LinkedList.SimpleLinkedList;
 
-public class SimpleLinkedList {
-    private Node head = null;
 
+public class SimpleLinkedList {
+    // add DONE!
+    // add value at index
+
+    // isEmpty DONE!
+
+    // print DONE!
+
+    // search value DONE!
+    // get value by index DONE!
+    // get index by value DONE!
+
+    // sort
+    // remove value
+
+
+    private Node head ;
 
     public void add(int value) {
-        Node newNode = new Node(value, null);
-        if (isEmpty()) {
-            head = newNode;
-        } else {
-            Node temp = head;
-            while (temp.next != null) {
-                temp = temp.next;
+        Node toBeAdded = new Node(value, null);
+        if(isEmpty()){
+            head = toBeAdded;
+        }else{
+            Node pointer = head;
+            while(pointer.next != null){
+                pointer = pointer.next;
             }
-            temp.next = newNode;
+            pointer.next = toBeAdded;
         }
+    }
+
+    public boolean isEmpty(){
+        if(head == null){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void print(){
+        Node pointer = head;
+        while(pointer.next != null){
+            System.out.print(pointer.value + " --> ");
+            pointer = pointer.next;
+        }
+        System.out.print(pointer.value + " --> null\n");
 
     }
 
-    public boolean isEmpty() {
-        if (head == null) {
+    public boolean search(int value){
+        if(isEmpty()){
+            return false;
+        }
+        if(head.value == value){
             return true;
+        }
+        Node pointer = head;
+        while(pointer.next != null){
+            if(pointer.value == value){
+                return true;
+            }
+            pointer = pointer.next;
         }
         return false;
     }
 
-    public void print() {
-        Node temp = head;
-        while (temp.next != null) {
-            System.out.print(temp.value + " --> ");
-            temp = temp.next;
+   public int getValueByIndex (int index){
+        if(isEmpty() || index < 0){
+            return -1;
+        }else{
+            if(index == 0){
+                return head.value;
+            }else{
+                Node pointer = head;
+                int i = 0;
+                while(i != index && pointer.next != null){
+                    pointer = pointer.next;
+                    i++;
+                }
+                if(i == index){
+                    return pointer.value;
+                }else{
+                    return -1;
+                }
+
+            }
         }
-        System.out.print(temp.value);
+   }
+
+    public int getIndexByValue (int value){
+        if(isEmpty()){
+            return -1;
+        }
+        if(value == head.value){
+            return 0;
+        }
+        Node pointer = head;
+        int i= 0;
+        while(pointer.next != null){
+            if(pointer.value == value){
+                return i;
+            }
+            pointer = pointer.next;
+            i++;
+        }
+        return -1;
     }
+
 }
+
+
